5b69a0166fa521b6a1998d4123a921e2d3dd24b7
Minsung Kim <mskim4048@naver.com>
2025-07-27 20:44:59 +0900

    Initial Commit

---
+++ Assignment-ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment-ShoppingList
+//
+//  Created by 김민성 on 7/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
