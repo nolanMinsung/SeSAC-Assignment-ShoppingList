7639410f7af11a9106b853d98dc123b39a57eeb0
Minsung Kim <mskim4048@naver.com>
2025-08-14 09:21:57 +0900

    feat: Swift Concurrency 방식의 서버 요청 함수(searchShopping) 함수 구현

--- Assignment-ShoppingList/Shopping/Network/ShoppingListNetworkService.swift
+++ Assignment-ShoppingList/Shopping/Network/ShoppingListNetworkService.swift
@@ -42,7 +42,17 @@ final class ShoppingListNetworkService {
         } catch {
             completion(.failure(error))
         }
-        
+    }
+    
+    // Swift Concurrency 방식으로도 구현해보았음. (아직 적용하진 않음.)
+    func searchShohpping<T: Decodable>(apiRouter: NaverOPENAPISearchRouter, type: T.Type) async throws -> T {
+        guard let url = URL(string: apiRouter.endpointString) else {
+            throw NetworkServiceError.invalidEndpoint(apiRouter.endpointString)
+        }
+        let headers = try apiRouter.headers
+        let request = AF.request(url, method: apiRouter.method, parameters: apiRouter.parameter, headers: headers)
+        let resultValue = try await request.serializingDecodable(T.self).value
+        return resultValue
     }
     
 }
